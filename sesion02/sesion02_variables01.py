ba65e43462d6acaa3f4082ca1d196fc8ed29b6dc
Paco Mastermind <paco@mastermind.ac>
2024-10-24 18:54:31 +0200

    Clase02. Variables

---
+++ sesion02/sesion02_variables01.py
@@ -0,0 +1,15 @@
+canastas = 7
+canastas_paco = 2
+canastas_pau = 8
+canastas_eloi = 7
+ptos_tres = 3
+
+
+total_puntos_paco = canastas_paco * ptos_tres
+total_puntos_pau = canastas_pau * ptos_tres
+total_puntos_eloi = canastas_eloi * ptos_tres
+
+print(total_puntos_paco)
+print(total_puntos_pau)
+print(total_puntos_eloi)
+
